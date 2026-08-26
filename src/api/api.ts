9faf0092552a278a1708502c9a6cc0ad6ebd7459
Anna0626 <lsr00626@naver.com>
2026-08-27 03:17:13 +0900

    feat: 미분류 UI 구현

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,9 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.anna-lee.xyz",
   // baseURL: "http://localhost:8080",
-  headers: {
-    "Content-Type": "application/json",
-  },
   withCredentials: true,
 });
 
